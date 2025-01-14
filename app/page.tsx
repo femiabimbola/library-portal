3118ceb2d850c546924992ab0dd953de67447002
femi abimbola <femiabimbola6@gmail.com>
2025-01-14 10:47:05 -0800

    added the public folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
+import Header from "@/components/Header";
 import { Button } from "@/components/ui/button";
 
-const  Home = () => {
+const Home = () => {
   return (
-    <>
-   <p> The home page</p>
-   <Button> HEY</Button>
-   </>
+    <main className="root-container">
+      <div className="mx-auto max-w-7xl">
+        <Header />
+        <p> The home page</p>
+      </div>
+    </main>
   );
-}
+};
 
-export default Home
+export default Home;
