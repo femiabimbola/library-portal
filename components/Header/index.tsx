1b2f0f82156198596cfd15a6178853dfd4c69dd5
femi abimbola <femiabimbola6@gmail.com>
2025-01-16 20:51:13 -0800

    working on the header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,12 +1,25 @@
+"use client"
+
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 const Header = () => {
+  // For active link to change colour
+  const pathname = usePathname()
   return (
    <header className=" my-10 flex justify-between gap-5">
       <Link href="/">
         <Image src="/icons/logo.svg" alt="logo" width={40} height={40} />
+        Bookwise
       </Link>
+      <ul className="flex flex-row items-center gap-8">
+        <li>
+          <Link href='/' className={cn("text-base cursor-pointer capitalize", pathname==='/library' ? 'text-light-200' : 'text-lime-100')}>       Library 
+          </Link>
+        </li>
+      </ul>
    </header>
   )
 }
