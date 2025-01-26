d6966ea4b5fdaafc4d8e24bf734a66331620136c
femi abimbola <femiabimbola6@gmail.com>
2025-01-26 13:14:06 -0800

    started the file upload

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,11 @@
+"use client"
+
 import AuthForm from "@/components/AuthForm"
 import {  signUpSchema } from "@/lib/validations"
 
 const SignInPage = () => {
   return (
-    <AuthForm type="SIGN_UP"
+    <AuthForm type="SIGN_IN"
     schema={signUpSchema}
     defaultValues={{
       email: "",
--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -12,6 +12,8 @@ import {
 } from "react-hook-form";
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { FIELD_NAMES, FIELD_TYPES } from "@/constants";
 import {
   Form,
   FormControl,
@@ -21,6 +23,8 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
+import FileUpload from "@/components/FileUpload";
 
 //  T is generic, it can take  the form of whatever is passed into it
 // T is the default values we are passing
@@ -65,10 +69,53 @@ const AuthForm = <T extends FieldValues>({
           onSubmit={form.handleSubmit(handleSubmit)}
           className="w-full space-y-6"
         >
-
+          {Object.keys(defaultValues).map((field) => (
+            <FormField 
+              key={field}  
+              control={form.control}
+              name={field as Path<T>}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="capitalize">
+                    {FIELD_NAMES[field.name as keyof typeof FIELD_NAMES]}
+                  </FormLabel>
+                  <FormControl>
+                  {field.name === "universityCard" ? (
+                      <FileUpload
+                        type="image"
+                        accept="image/*"
+                        placeholder="Upload your ID"
+                        folder="ids"
+                        variant="dark"
+                        onFileChange={field.onChange}
+                      />
+                    ) : (
+                      <Input
+                        required
+                        type={
+                          FIELD_TYPES[field.name as keyof typeof FIELD_TYPES]
+                        }
+                        {...field}
+                        className="form-input"
+                      />
+                    )}
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          ))}
         </form>
-
       </Form>
+      <p className="text-center text-base font-medium">
+        {isSignIn ? "New to BookWise? " : "Already have an account? "}
+
+        <Link
+          href={isSignIn ? "/sign-up" : "/sign-in"}
+          className="font-bold text-primary"
+        >
+          {isSignIn ? "Create an account" : "Sign in"}
+        </Link>
+      </p>
     </div>
   );
 };
