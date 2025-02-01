369c83dda9c51fd076d9aea4c6731c1516c50fa7
femi abimbola <femiabimbola6@gmail.com>
2025-01-31 23:44:46 -0800

    working on the authentication

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -11,9 +11,14 @@ const {
 
 const authenticator = async () => {
   try {
-    
-  } catch (error) {
-    
+    const response = await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`);
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(`Request failed with status ${response.status}: ${errorText}`,);
+    }
+  } catch (error: any) {
+    throw new Error(`Authentication request failed: ${error.message}`)
   }
 }
 
