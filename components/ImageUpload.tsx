c351a3677175c90f8294b4077fb66f00b78d095c
femi abimbola <femiabimbola6@gmail.com>
2025-02-01 01:38:49 -0800

    still working on the image upload

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { IKImage, IKVideo, ImageKitProvider, IKUpload,} from "imagekitio-next";
+import { IKImage, IKVideo, ImageKitProvider, IKUpload } from "imagekitio-next";
 import config from "@/lib/config";
+import { useRef, useState } from "react";
+import Image from "next/image";
 
 const {
   env: {
@@ -15,17 +17,75 @@ const authenticator = async () => {
 
     if (!response.ok) {
       const errorText = await response.text();
-      throw new Error(`Request failed with status ${response.status}: ${errorText}`,);
+      throw new Error(
+        `Request failed with status ${response.status}: ${errorText}`
+      );
     }
+    const data = await response.json();
+    const { signature, expire, token } = data;
+
+    return { token, expire, signature };
   } catch (error: any) {
-    throw new Error(`Authentication request failed: ${error.message}`)
+    throw new Error(`Authentication request failed: ${error.message}`);
   }
+};
+
+interface Props {
+  type: "image" | "video";
+  accept: string;
+  placeholder: string;
+  folder: string;
+  variant: "dark" | "light";
+  onFileChange: (filePath: string) => void;
+  value?: string;
 }
 
-const ImageUpload = () => {
+const ImageUpload = ({ value }: Props) => {
+  const ikUploadRef = useRef(null);
+  const [progress, setProgress] = useState(0);
+  const [file, setFile] = useState<{ filePath: string | null }>({
+    filePath: value ?? null,
+  });
+
+  const onError = () => {};
+  const onSuccess = () => {};
+
   return (
-    <div>FileUpload</div>
-  )
-}
+    <ImageKitProvider
+      publicKey={publicKey}
+      urlEndpoint={urlEndpoint}
+      authenticator={authenticator}
+    >
+      <IKUpload
+        ref={ikUploadRef}
+        onError={onError}
+        onSuccess={onSuccess}
+        useUniqueFileName={true}
+        validateFile={onValidate}
+        onUploadStart={() => setProgress(0)}
+        onUploadProgress={({ loaded, total }) => {
+          const percent = Math.round((loaded / total) * 100);
+          setProgress(percent);
+        }}
+        folder={folder}
+        accept={accept}
+        className="hidden"
+      />
 
-export default ImageUpload
+      <button className={"upload-btn"}>
+        <Image
+          src="/icons/upload.svg"
+          alt="upload-icon"
+          width={20}
+          height={20}
+          className="object-contain"
+        />
+        <p className="text-base text-light-100"> Upload a File</p>
+
+        {file && <p className="upload-filename">{file.filePath}</p>}
+      </button>
+    </ImageKitProvider>
+  );
+};
+
+export default ImageUpload;
