9f9385275ffd82a54fdf87f82593a250e8511fe7
femi abimbola <femiabimbola6@gmail.com>
2025-02-15 01:23:23 -0800

    working on the working

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,10 +2,15 @@ import Header from '@/components/Header'
 import  { ReactNode } from 'react'
 import { auth } from "@/auth";
 import { redirect } from 'next/navigation';
+import { after } from 'next/server';
 
 const Layout = async({children}:{children: ReactNode}) => {
    const session = await auth();
     if(!session) redirect("/sign-in")
+    
+    after(async () => {
+      if(!session?.user?.id) return;
+    })
 
   return (
     <main className="root-container">
--- lib/workflow.ts
+++ lib/workflow.ts
@@ -10,3 +10,24 @@ export const workflowClient = new WorkflowClient({
 const qstashClient = new QStashClient({
   token: config.env.upstash.qstashToken,
 });
+
+export const sendEmail = async ({
+  email, subject, message,
+}: {
+  email: string;
+  subject: string;
+  message: string;
+}) => {
+  await qstashClient.publishJSON({
+    api: {
+      name: "email",
+      provider: resend({ token: config.env.resendToken }),
+    },
+    body: {
+      from: "Library <t.comush@academy>",
+      to: [email],
+      subject,
+      html: message,
+    },
+  });
+};
