0fdccf7dca6a8b0a241ce0d0f963e928634ea5ba
femi abimbola <femiabimbola6@gmail.com>
2025-02-14 23:21:12 -0800

    working on the workflow

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,7 +1,12 @@
 import config from "@/lib/config";
 import { Client as WorkflowClient } from "@upstash/workflow";
+import { Client as QStashClient, resend } from "@upstash/qstash"
 
 export const workflowClient = new WorkflowClient({
   baseUrl: config.env.upstash.qstashUrl,
+  token: config.env.upstash.qstashToken,
+});
+
+const qstashClient = new QStashClient({
   token: config.env.upstash.qstashToken,
 });
